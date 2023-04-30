4823cc33e28dac08323ce24214b7d0141d851821
Ramazan <r.tokoshev@gmail.com>
2023-04-30 19:49:05 +0600

    #3 feat: create a NavBar, adding linnks scrolls

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Nav.css';
+import {BiHomeHeart, BiBook, BiMessageSquareDetail} from "react-icons/bi";
+import {AiOutlineUser} from "react-icons/ai";
+import {RiServiceLine} from "react-icons/ri";
+
 const Nav = () => {
+	const [activeNav, setActiveNav] = useState('#');
+
 	return (
-			<div>
-				Nav
-			</div>
+			<nav>
+				<a href="#"
+				   onClick={() => setActiveNav('#')}
+				className={activeNav === '#' ? 'active' : ''}
+				><BiHomeHeart/></a>
+				<a href="#about"
+				   onClick={() => setActiveNav('#about')}
+				   className={activeNav === '#about' ? 'active' : ''}
+				><AiOutlineUser/></a>
+				<a href="#experience"
+				   onClick={() => setActiveNav('#experience')}
+				   className={activeNav === '#experience' ? 'active' : ''}
+				><BiBook/></a>
+				<a href="#services"
+				   onClick={() => setActiveNav('#services')}
+				   className={activeNav === '#services' ? 'active' : ''}
+				><RiServiceLine/></a>
+				<a href="#contact"
+				   onClick={() => setActiveNav('#contact')}
+				   className={activeNav === '#contact' ? 'active' : ''}
+				><BiMessageSquareDetail/></a>
+			</nav>
 	);
 };
 

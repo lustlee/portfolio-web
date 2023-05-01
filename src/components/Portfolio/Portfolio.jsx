c960c8a3b23445c0254552e0873e64d7caf8b45e
Ramazan <r.tokoshev@gmail.com>
2023-05-01 20:27:42 +0600

    #6 feat: finish section portfolio, create page constants for using data info portfolio

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
 import './Portfolio.css';
+
+import data from '../../constants/main-constants';
 const Portfolio = () => {
 	return (
 			<section id='portfolio'>
-				Portfolio
+				<h5>My Recent Work</h5>
+				<h2>Portfolio</h2>
+
+				<div className="container portfolio__container">
+					{
+						data.map(({id, image, title, github, demo}) => {
+							return (
+									<article key={id} className='portfolio__item'>
+										<div className='portfolio__item-image'>
+											<img src={image} alt={title}/>
+										</div>
+										<h3>{title}</h3>
+										<div className="portfolio__item-cta">
+											<a href={github} className='btn' target='_blank'>GitHub</a>
+											<a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+										</div>
+									</article>
+							)
+						})
+					}
+				</div>
+
 			</section>
 	);
 };
---
+++ src/constants/main-constants.js
@@ -0,0 +1,53 @@
+import IMG1 from "../assets/portfolio1.jpg";
+import IMG2 from "../assets/portfolio2.jpg";
+import IMG3 from "../assets/portfolio3.jpg";
+import IMG4 from "../assets/portfolio4.jpg";
+import IMG5 from "../assets/portfolio5.png";
+import IMG6 from "../assets/portfolio6.jpg";
+
+const dataPortfolio = [
+	{
+		id: 1,
+		image: IMG1,
+		title: 'Crypto Some text',
+		github: 'https://github.com',
+		demo: 'https://github.com/lustlee'
+	},
+	{
+		id: 2,
+		image: IMG2,
+		title: 'Crypto Some text 2',
+		github: 'https://github.com',
+		demo: 'https://github.com/lustlee'
+	},
+	{
+		id: 3,
+		image: IMG3,
+		title: 'Crypto Some text 3',
+		github: 'https://github.com',
+		demo: 'https://github.com/lustlee'
+	},
+	{
+		id: 4,
+		image: IMG4,
+		title: 'Crypto Some text 4 ',
+		github: 'https://github.com',
+		demo: 'https://github.com/lustlee'
+	},
+	{
+		id: 5,
+		image: IMG5,
+		title: 'Crypto Some text 5',
+		github: 'https://github.com',
+		demo: 'https://github.com/lustlee'
+	},
+	{
+		id: 6,
+		image: IMG6,
+		title: 'Crypto Some text 6',
+		github: 'https://github.com',
+		demo: 'https://github.com/lustlee'
+	},
+];
+
+export default dataPortfolio;

d0937ac946e7c0b54f198f5fe30466d750667a14
Ramazan <r.tokoshev@gmail.com>
2023-05-02 01:03:18 +0600

    #7 feat: finish component contact, create form, added logic for sent message in my email

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,49 @@
 import React from 'react';
 import './Contact.css';
+import {MdEmail} from "react-icons/md";
+import {FaTelegramPlane} from "react-icons/fa";
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+	const form = useRef();
+	const sendEmail = (e) => {
+		e.preventDefault();
+
+		emailjs.sendForm('service_jxirrcm', 'template_v96byvp', form.current, 'ZnoHuByC1wY4oWa2P');
+
+		e.target.reset();
+	};
+
 	return (
 			<section id='contact'>
-				Contact
+				<h5>Get in Touch</h5>
+				<h2>Contact me</h2>
+
+				<div className="container contact__container">
+					<div className='contact__options'>
+						<article className="contact__option">
+							<MdEmail className='contact__option-icon'/>
+							<h4>Email</h4>
+							<h5>r.tokoshev@gmail.com</h5>
+							<a href="mailto:r.tokoshev@gmail.com">Send a message</a>
+						</article>
+						<article className="contact__option">
+							<FaTelegramPlane className='contact__option-icon'/>
+							<h4>Telegram</h4>
+							<h5>@ARAMUSHI</h5>
+							<a href="https://t.me/aramushi" target='_blank'>Send a message</a>
+						</article>
+					</div>
+
+					<form ref={form} onSubmit={sendEmail}>
+						<input type="text" name='name' placeholder='Your Full Name' required/>
+						<input type="email" name='email' placeholder='Your Email'/>
+						<textarea name="message" rows="7" placeholder='Your Message'></textarea>
+						<button type='submit' className='btn btn-primary'>Send Message</button>
+					</form>
+
+				</div>
 			</section>
 	);
 };

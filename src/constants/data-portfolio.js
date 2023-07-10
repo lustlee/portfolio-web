f614e1ab92833077f60c6154e6a5de39fe80f4c8
Ramazan <r.tokoshev@gmail.com>
2023-07-10 16:57:51 +0600

    feat: add new project in portfolio

--- src/constants/data-portfolio.js
+++ src/constants/data-portfolio.js
@@ -1,5 +1,5 @@
-import IMG1 from "../assets/portfolio1.jpg";
-import IMG2 from "../assets/portfolio2.jpg";
+import IMG1 from "../assets/portfolio-1.jpg";
+import IMG2 from "../assets/portfolio-2.jpg";
 import IMG3 from "../assets/portfolio3.jpg";
 import IMG4 from "../assets/portfolio4.jpg";
 import IMG5 from "../assets/portfolio5.png";
@@ -9,16 +9,16 @@ const dataPortfolio = [
 	{
 		id: 1,
 		image: IMG1,
-		title: 'Crypto Some text',
-		github: 'https://github.com',
-		demo: 'https://github.com/lustlee'
+		title: 'Countries App',
+		github: 'https://github.com/lustlee/countries-app',
+		demo: 'https://countries-app-gilt-delta.vercel.app/'
 	},
 	{
 		id: 2,
 		image: IMG2,
-		title: 'Crypto Some text 2',
-		github: 'https://github.com',
-		demo: 'https://github.com/lustlee'
+		title: 'Photo App',
+		github: 'https://github.com/lustlee/photo_app',
+		demo: 'https://photo-app-omega-orcin.vercel.app/'
 	},
 	{
 		id: 3,
